7865ccee0f940198958b0c346440e5d52fc698a3
Simeon <beglpumba@gmail.com>
2026-03-28 20:03:13 +0300

    test: add unit tests for postService and authService

---
+++ src/__tests__/unit/auth.service.test.ts
@@ -0,0 +1,152 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import * as userRepository from '../../repositories/userRepository';
+import * as authService from '../../services/authService';
+import AppError from '../../utils/AppError';
+
+jest.mock('bcrypt');
+jest.mock('jsonwebtoken');
+jest.mock('../../repositories/userRepository');
+
+describe('authService', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
+    describe('register', () => {
+        const users = [ 
+                { name: 'Ivan2', email: 'ivan13@mail.ru', password: '1234', role: 'user' },
+                { name: 'Ivan3', email: 'ivan14@mail.ru', password: '1234', role: 'user' }
+            ]
+        it('should return created user', async () => {
+
+            const createdUser = {
+                name: 'Ivan',
+                email: 'ivan12@mail.ru',
+                role: 'user',
+                toObject() {
+                    return {
+                        name: this.name,
+                        email: this.email,
+                        role: this.role,
+                        password: 'hashed'
+                    };
+                }
+            };
+
+            const expectedResult = {
+                name: 'Ivan',
+                email: 'ivan12@mail.ru',
+                role: 'user',
+            };
+
+            const hashedPassword = '!@#$';
+
+            (userRepository.create as jest.Mock).mockResolvedValue(createdUser);
+            (bcrypt.hash as jest.Mock).mockResolvedValue(hashedPassword);
+
+
+            const result = await authService.register({
+                name: 'Ivan',
+                email: 'ivan12@mail.ru',
+                password: '1234' 
+            })
+        
+            expect(userRepository.create).toHaveBeenCalledWith({
+                name: 'Ivan',
+                email: 'ivan12@mail.ru',
+                password: '!@#$' 
+            });
+            expect(result).toEqual(expectedResult);
+        })
+
+        it('should throw AppError 404 if user with such email have existed yet', async () => {
+            (userRepository.findByEmail as jest.Mock).mockResolvedValue({ email: 'ivan13@mail.ru' });
+
+            await expect(authService.register({
+                name: 'Ivan',
+                email: 'ivan13@mail.ru',
+                password: '1234'
+            })).rejects.toThrow(AppError);
+
+            await expect(authService.register({
+                name: 'Ivan',
+                email: 'ivan13@mail.ru',
+                password: '1234'
+            })).rejects.toMatchObject({
+                statusCode: 400,
+                message: 'Email already exist'
+            });
+        })
+    })
+
+    describe('login', () => {
+
+        const users = [ 
+                { name: 'Ivan', email: 'ivan12@mail.ru', password: '1234', role: 'user' },
+                { name: 'Ivan2', email: 'ivan13@mail.ru', password: '1234', role: 'user' }
+            ];
+
+        it('shold return user founded by token', async () => {
+
+            const user = {
+                email: 'ivan12@mail.ru',
+                password: '1234'
+            };
+
+            const returnedUser = {
+                _id: "65a8...",
+                name: "John",
+                email: "john@mail.com",
+                password: "$2b$10$..."
+            }
+            const boolean = true;
+
+            (userRepository.findByEmail as jest.Mock).mockResolvedValue(returnedUser);
+            (bcrypt.compare as jest.Mock).mockResolvedValue(boolean);
+            (jwt.sign as jest.Mock).mockReturnValue('@123');
+
+            const result = await authService.login(user);
+
+            expect(userRepository.findByEmail).toHaveBeenCalledWith(user.email);
+            expect(result).toEqual({ token: '@123' });
+        })
+
+        it('should throw if email not found', async () => {
+            (userRepository.findByEmail as jest.Mock).mockResolvedValue(null);
+
+            await expect(authService.login({
+                email: 'ivan1100@mail.ru',
+                password: '1234'
+            })).rejects.toThrow(AppError);
+
+            await expect(authService.login({
+                email: 'ivan1100@mail.ru',
+                password: '1234'
+            })).rejects.toMatchObject({
+                statusCode: 404,
+                message: 'Invalid email or password'
+            });
+        });
+
+        it('should throw if password is wrong', async () => {
+            (userRepository.findByEmail as jest.Mock).mockResolvedValue({ email: 'ivan12@mail.ru', password: '$2b$10$...' });
+            (bcrypt.compare as jest.Mock).mockResolvedValue(false);
+            
+            await expect(authService.login({
+                email: 'ivan12@mail.ru',
+                password: '1234567'
+            })).rejects.toThrow(AppError);
+
+            await expect(authService.login({
+                email: 'ivan12@mail.ru',
+                password: '1234567'
+            })).rejects.toMatchObject({
+                statusCode: 404,
+                message: 'Invalid email or password'
+            });
+        });
+    })
+})
+
+
--- src/__tests__/unit/post.service.test.ts
+++ src/__tests__/unit/post.service.test.ts
@@ -1,11 +1,46 @@
 import * as postRepository from '../../repositories/postRepository';
-import { create } from '../../services/postService';
+import * as postService from '../../services/postService';
+import AppError from '../../utils/AppError';
 
 jest.mock('../../repositories/postRepository');
 
 describe('PostService', () => {
     beforeEach(() => {
         jest.clearAllMocks();
+    }); 
+
+    describe('getAll', () => {
+        it('should return all posts', async() => {
+            const mockPosts = [{ _id: '1', title: 'Post 1' }];
+            (postRepository.findAll as jest.Mock).mockResolvedValue(mockPosts);
+
+            const result = await postService.getAll();
+
+            expect(postRepository.findAll).toHaveBeenCalledTimes(1);
+            expect(result).toEqual(mockPosts);
+        });
+    });
+
+    describe('getById', () => {
+        it('should return a post by id', async() => {
+            const mockPost = { _id: "123", title: "Test"};
+            (postRepository.findById as jest.Mock).mockResolvedValue(mockPost);
+
+            const result = await postService.getById('123');
+
+            expect(postRepository.findById).toHaveBeenCalledWith('123');
+            expect(result).toEqual(mockPost);
+        });
+
+        it('should throw AppError 404 if post not found', async() => {
+            (postRepository.findById as jest.Mock).mockResolvedValue(null);
+
+            await expect(postService.getById('999')).rejects.toThrow(AppError);
+            await expect(postService.getById('999')).rejects.toMatchObject({
+                statusCode: 404,
+                message: 'Post not found',
+            });
+        });
     });
 
     describe('create', () => {
@@ -15,18 +50,56 @@ describe('PostService', () => {
 
             (postRepository.create as jest.Mock).mockResolvedValue(expectedPost);
 
-            const result = await create(inputData);
+            const result = await postService.create(inputData);
 
             expect(postRepository.create).toHaveBeenCalledWith(inputData);
             expect(result).toEqual(expectedPost);
         });
         
         it('should throw an error if repository fails', async () => {
-            const inputData = { title: 'Test Post', content: 'Hello World' };
-
             (postRepository.create as jest.Mock).mockRejectedValue(new Error('DB error'));
 
-            await expect(create(inputData)).rejects.toThrow('DB error');
+            await expect(postService.create({})).rejects.toThrow('DB error');
         })
+    });
+
+    describe('update', () => {
+        it('should update and return the post', async() => {
+            const updated = { _id: '123', title: 'Updated' };
+            (postRepository.update as jest.Mock).mockResolvedValue(updated);
+
+            const result = await postService.update('123', {title: '123'});
+
+            expect(postRepository.update).toHaveBeenCalledWith('123', {title: '123'});
+            expect(result).toEqual(updated);
+        })
+
+        it('should throw AppError 404 if post not found', async () => {
+            (postRepository.update as jest.Mock).mockResolvedValue(null);
+
+            await expect(postService.update('999', {})).rejects.toMatchObject({
+                statusCode: 404,
+            });
+        });
+    });
+
+    describe('remove', () => {
+        it('should delete and return the post', async () => {
+            const deleted = {_id: '123', title: 'toDelete'};
+            (postRepository.remove as jest.Mock).mockResolvedValue(deleted);
+
+            const result = await postService.remove('123');
+
+            expect(postRepository.remove).toHaveBeenCalledWith('123');
+            expect(result).toEqual(deleted);
+        })
+
+        it('should throw AppError 404 if post not found', async () => {
+            (postRepository.remove as jest.Mock).mockResolvedValue(null);
+
+            await expect(postService.remove('999')).rejects.toMatchObject({
+                statusCode: 404,
+            });
+        });
     });
 });

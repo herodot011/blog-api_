c93b2654cba0a247453de88c03daa567777e4a64
Simeon <beglpumba@gmail.com>
2026-04-06 01:30:50 +0300

    feat: add morgan request logging

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 import helmet from 'helmet';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import express from 'express';
 import postsRouter from './routes/posts';
@@ -19,6 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+app.use(morgan('dev'));
 
 app.use('/posts', postsRouter);
 app.use('/categories', categoriesRouter);
